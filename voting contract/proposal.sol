242481d2cfe33a149ea3a20cc07f499575428c1f
Abdisa Reta <abdisaretaintale@gmail.com>
2026-05-25 23:14:22 +0300

    add new Solidity contract files for cast_a_vote, execute, members, multipile_vote, proposal, and voting_event

---
+++ voting contract/proposal.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+    }
+    
+    // TODO: create a public array of type Proposal and call it proposals
+    Proposal[] public proposals;
+
+    function newProposal(address target, bytes calldata data) external {
+        // TODO: create a new Proposal and add it to the proposals array
+        Proposal memory newProposal = Proposal({
+            target: target,
+            data: data,
+            yesCount: 0,
+            noCount: 0
+        });
+        proposals.push(newProposal);
+    }
+}
---
+++ voting contract/voting_event.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+    }
+    
+    // TODO: create a public array of type Proposal and call it proposals
+    Proposal[] public proposals;
+    
+    // Track votes: proposalId => voter => hasVoted (bool)
+    mapping(uint => mapping(address => bool)) public hasVoted;
+    // Track vote choice: proposalId => voter => supported (bool)
+    mapping(uint => mapping(address => bool)) public voteChoice;
+
+    // Events
+    event ProposalCreated(uint proposalId);
+    event VoteCast(uint proposalId, address voter);
+
+    function newProposal(address target, bytes calldata data) external {
+        // TODO: create a new Proposal and add it to the proposals array
+        Proposal memory newProposal = Proposal({
+            target: target,
+            data: data,
+            yesCount: 0,
+            noCount: 0
+        });
+        uint proposalId = proposals.length;
+        proposals.push(newProposal);
+        emit ProposalCreated(proposalId);
+    }
+
+    function castVote(uint proposalId, bool _supports) external {
+        require(proposalId < proposals.length, "Invalid proposal ID");
+        
+        // Check if the voter has already voted on this proposal
+        if (hasVoted[proposalId][msg.sender]) {
+            // Get their previous vote
+            bool previousVote = voteChoice[proposalId][msg.sender];
+            
+            // If the vote is changing, adjust counts
+            if (previousVote != _supports) {
+                if (_supports) {
+                    // Changing from no to yes
+                    proposals[proposalId].noCount--;
+                    proposals[proposalId].yesCount++;
+                } else {
+                    // Changing from yes to no
+                    proposals[proposalId].yesCount--;
+                    proposals[proposalId].noCount++;
+                }
+                // Update their vote choice
+                voteChoice[proposalId][msg.sender] = _supports;
+            }
+            // If same vote, do nothing (no change to counts)
+        } else {
+            // First time voting
+            if (_supports) {
+                proposals[proposalId].yesCount++;
+            } else {
+                proposals[proposalId].noCount++;
+            }
+            hasVoted[proposalId][msg.sender] = true;
+            voteChoice[proposalId][msg.sender] = _supports;
+        }
+        
+        // Emit event for every vote cast
+        emit VoteCast(proposalId, msg.sender);
+    }
+}
